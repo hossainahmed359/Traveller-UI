2042d3d6479121b1f5dad45706f3d6adc745387a
hossainahmed359 <hossainahmed359@gmail.com>
2021-10-31 23:40:07 +0600

    404 page eidted

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,8 +4,8 @@ import React from 'react';
 const NotFound = () => {
 
     return (
-        <div>
-            <h1>404</h1>
+        <div className=''>
+            <img className="w-100" src="https://i.ibb.co/kMcBSpQ/2488755-ai.jpg" alt="" />
 
         </div>
     );
